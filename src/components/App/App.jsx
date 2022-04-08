c1089c7f0e1029bf01a8497895e8787c2719e942
SharonJseg <sharon.j.segal@gmail.com>
2022-04-08 20:42:41 +0300

    Completed Gallery

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -48,7 +48,7 @@ function App() {
           <div className='your-day'>
             <div className='your-day__content'>
               <h2 className='your-day__title heading-m'>
-                Your Day at the Gallery
+                Your Day <span>at the Gallery </span>
               </h2>
               <p className='your-day__text text-m'>
                 Wander through our distinct collections and find new insights
@@ -56,25 +56,29 @@ function App() {
                 process.
               </p>
             </div>
-            <img
-              className='your-day__image'
-              src={galleryImage1}
-              alt='gallery image'
-            />
+            <div className='your-day__image-container'>
+              <img
+                className='your-day__image'
+                src={galleryImage1}
+                alt='gallery image'
+              />
+            </div>
           </div>
           <div className='inspired'>
-            <img
-              className='inspired__image-l'
-              src={galleryImage2}
-              alt='gallery image'
-            />
-            <div className='ispired__column'>
+            <div className='inspired__column_one'>
+              <img
+                className='inspired__image-l'
+                src={galleryImage2}
+                alt='gallery image'
+              />
+            </div>
+            <div className='inspired__column_two'>
               <img
                 className='inspired__image-m'
                 src={galleryImage3}
                 alt='gallery image'
               />
-              <div className='inpired__content '>
+              <div className='inpired__content'>
                 <h2 className='inspired__title heading-m'>
                   COME & <br /> BE INSPIRED
                 </h2>
