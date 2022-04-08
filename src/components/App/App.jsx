4a16f6c71b743f61e08be1c4d1aac1837cde61a5
SharonJseg <sharon.j.segal@gmail.com>
2022-04-08 22:27:46 +0300

    Added leaflet to project

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,34 @@
 import './App.css';
-
+import arrowLeft from '../../images/icon-arrow-left.svg';
 import Header from '../Header/Header';
 import Gallery from '../Gallery/Gallery';
 import Footer from '../Footer/Footer';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 function App() {
+  const position = [32.17591, 34.91028];
+
   return (
     <>
-      <Header />
+      <header>
+        <section className='location' id='map'>
+          {/* <a href='#' className='location__button'>
+            <img src={arrowLeft} alt='' className='location__button-icon' />
+            our location
+          </a> */}
+          <MapContainer center={position} zoom={13} zoomControl={false}>
+            <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+            <Marker position={position}>
+              <Popup>What's up sharon?</Popup>
+            </Marker>
+          </MapContainer>
+        </section>
+      </header>
+
+      {/* <Header />
       <main>
         <Gallery />
-      </main>
+      </main> */}
       <Footer />
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import ReactDOMClient from 'react-dom/client';
+// import ReactDOMClient from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
-const container = document.getElementById('root');
-const root = ReactDOMClient.createRoot(container);
+// const container = document.getElementById('root');
+// const root = ReactDOMClient.createRoot(container);
 
-root.render(<App tab='home' />);
+// root.render(<App tab='home' />);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
